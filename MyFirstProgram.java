cab42fa74ad687288ed64f72fb3919677b74152d
anishcb <142503391+anishcb@users.noreply.github.com>
2025-02-18 09:48:30 +0530

    Add files via upload

---
+++ MyFirstProgram.java
@@ -0,0 +1,9 @@
+class Anish
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello world");
+  }
+}
+
+
---
+++ MySecondProgram.java
@@ -0,0 +1,7 @@
+class Anish
+{
+  public static void main(String[] args)
+  {
+     System.out.println("Hello world");
+  }
+}
